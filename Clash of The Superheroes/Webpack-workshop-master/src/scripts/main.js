a9a8bcfc82c50ce0c45bd32a00366601963be58b
lukarostan <luka.rostan@factory.hr>
2020-07-08 20:58:11 +0200

    cleaned up code

--- Clash of The Superheroes/Webpack-workshop-master/src/scripts/main.js
+++ Clash of The Superheroes/Webpack-workshop-master/src/scripts/main.js
@@ -1,7 +1,6 @@
 const { get } = require("jquery");
 var heroes = new Array;
 const sendRequest = (method, url) => {
-    
     let req = new XMLHttpRequest();
     req.open(method, url);
     req.responseType = 'json';
@@ -12,25 +11,19 @@ const sendRequest = (method, url) => {
     return heroes;
 }
 
- const getHeroes = () => {  //Salje request za 8 random heroja
+ const getHeroes = () => { 
     for(let i=0; i<8;i++){
         var id = Math.floor(Math.random()*733);
          sendRequest('get','https://superheroapi.com/api/2983210371755036/'+id);
-    } 
-    
+    }
 } 
 getHeroes();
-console.log(heroes);
-
-
 
 function battle(i1, i2){
-/* console.log("i1s =  "+heroes[i1].powerstats.strength+"    i2s =   "+heroes[i2].powerstats.strength);
- */ 
-    if(heroes[i1].powerstats.strength > heroes[i2].powerstats.strength){
+    if((heroes[i1].powerstats.strength + heroes[i1].powerstats.intelligence) > (heroes[i2].powerstats.strength + heroes[i1].powerstats.intelligence)){
         heroes[i1].score=1;
         heroes[i2].score=0;
-    }else if(heroes[i1].powerstats.strength < heroes[i2].powerstats.strength){
+    }else if((heroes[i1].powerstats.strength + heroes[i1].powerstats.intelligence) < (heroes[i2].powerstats.strength + heroes[i1].powerstats.intelligence)){
         heroes[i2].score=1;
         heroes[i1].score=0;
     }else{
@@ -40,70 +33,36 @@ function battle(i1, i2){
     console.log(heroes[i1].name  +' - '+heroes[i2].name +'  '+heroes[i1].score+  ' : '+ heroes[i2].score);
 }
 
-
-
-
 setTimeout(function tournament () {
-    /*  var stat1 =  Math.floor(Math.random() * (7 - 1) + 1);
-    var stat2 =  Math.floor(Math.random() * (7 - 1) + 1);
-    switch(stat1){
-        case 1:
-            switch(stat2){
-                case 1: heroes[0].battleScore = heroes[0].powerstats.Intelligence + heroes[0].powerstats.Intelligence;
-                break;
-                    
-                }
-        break;
-    }  */
 
-    
-    console.log("%c ****Četvrtfinale****","color:#a7c0cd;");
+    console.log("%c ****Četvrtfinale****","color:#1976d2;");
         battle(0,1);
         battle(2,3);
         battle(4,5);
         battle(6,7);
         
-    console.log("%c ****Polufinale****","color:#a7c0cd;");
+    console.log("%c ****Polufinale****","color:#1976d2;");
         
      function removeLosers(heroes){
         return heroes.score!=0;
         }
         heroes = heroes.filter(removeLosers);
         
-        /* console.log(heroes); */
-
         battle(0,1);
         battle(2,3);
     
-    console.log("%c ****Finale****","color:#a7c0cd;");
+    console.log("%c ****Finale****","color:#1976d2;");
         heroes = heroes.filter(removeLosers);
             
-        /* console.log(heroes); */
-
         battle(0,1);
         
         heroes = heroes.filter(removeLosers);
         if(heroes[0] == undefined){
             console.log("It's a draw!");
+            alert("It's a draw!");
         }else{
 
-        
         console.log(heroes[0].name +" is winner!");
         }
-        
-
+        alert(heroes[0].name+" is winner!")
 }, 1500);
-
-
-/* 
-Intelligence:1
-Strength:2
-Speed:3
-Durability:4
-Power:5
-Combat:6 
-
-
-
-*/
-
