e8a7ebf5ace3150bd2be34bd4fca3897ec1ad80f
lukarostan <luka.rostan@factory.hr>
2020-07-31 18:05:22 +0200

    31.7.

--- COTH2/Webpack-workshop-master/src/scripts/main.js
+++ COTH2/Webpack-workshop-master/src/scripts/main.js
@@ -12,41 +12,21 @@ import './parts/jQuery';
 import './pages/user-page';
 
 
-
-var heroN = 1;
-var heroUser = 1;
-var tournamentHeroes = new Array;
+//##########INTERFACE#########//
 
 $(".logo").click(function(){
     window.location ="http://www.factory.hr";
 })
 
+
 $(".button").click(function(){
     $(".button").css("visibility","hidden");
     $(".main-container").css("display","none");
     $(".tournament-container").css("display", "flex");
-    
-    fillTounament();
-})
-
-function selectHeroes(){
-    tournamentHeroes = [];
     heroUser = heroN;
-    tournamentHeroes[0] = heroes[heroUser];
-    for(let i=0;i<7;i++){
-        var j = Math.floor(Math.random() * (10 - 1) + 1);
-        tournamentHeroes.push(heroes[j]);
-    }
-    console.log(tournamentHeroes);
-    return tournamentHeroes;
-}
-
-function fillTounament(){
-    selectHeroes();
-    for(let i = 1;i<9;i++){
-        $(".tournament-container .hero"+(i+1)).attr("src",tournamentHeroes[i].image.url);
-    }
-}
+    fillTournament();
+    
+})
 
 
 
@@ -58,8 +38,6 @@ $(".header h1").click(function(){
 
 
 
-console.log(heroN);
-
 const updateSelection = () => {
     $(".hero-card").css("display","none")
     $(".hero-card:nth-child("+heroN+")").css("display","flex")
@@ -67,8 +45,9 @@ const updateSelection = () => {
 }
 
 const updateRow = () =>{
-    $(".hero-row:nth-child("+ heroN+")").css("filter","grayscale(0%)")
+    $(".hero-row:nth-child("+ heroN+")").css("filter","grayscale(0%)");
 }
+
 
 
 $(".hero-button").click(function(){
@@ -132,86 +111,11 @@ $(".random").on("mouseup",function(){
 })
 
 $(".random").click(function(){
-    
-    
     heroN= Math.floor(Math.random() * (10 - 1) + 1);
     console.log("random "+heroN);
     updateSelection();
     
-    
 })
-
-
-
-const { get } = require("jquery");
-var heroes = new Array;
-
-
-function getHeroes(){
-    for(let i=0; i<10 ;i++){ 
-        var id = Math.floor(Math.random()*733);
-        $.getJSON("https://superheroapi.com/api/2983210371755036/"+ id , (data, status) => {
-             
-                heroes.push(data);
-              
-        })
-            .done(function(data){
-                let j = 1
-                $(".hero"+(j+i)).attr("src",data.image.url);
-                $(".hero"+(j+i)).attr("alt",data.name);
-                $(".hero"+(j+i)+"-name").text(data.name);
-                
-            })
-            
-    } 
-}
-getHeroes();
-console.log(heroes);
-
-function round(i1, i2){
-    const powerstats = ["combat","durability","intelligence","power","speed","strength"]
-    
-
-}
-
-/* setTimeout(function tournament () {
-
-    console.log("%c ****Četvrtfinale****","color:#1976d2;");
-        round(0,1);
-        round(2,3);
-        round(4,5);
-        round(6,7);
-        
-    console.log("%c ****Polufinale****","color:#1976d2;");
-        
-     function removeLosers(heroes){
-        return heroes.score!=0;
-        }
-        heroes = heroes.filter(removeLosers);
-        
-        round(0,1);
-        round(2,3);
-    
-    console.log("%c ****Finale****","color:#1976d2;");
-        heroes = heroes.filter(removeLosers);
-            
-        round(0,1);
-        
-        heroes = heroes.filter(removeLosers);
-        if(heroes[0] == undefined){
-            console.log("It's a draw!");
-            alert("It's a draw!");
-        }else{
-
-        console.log(heroes[0].name +" is winner!");
-        }
-        alert(heroes[0].name+" is winner!")
-}, 1500);
-  */
-
-
-
-
 
 
 
@@ -256,3 +160,125 @@ $(".hero-row .hero10").click(function(){
     updateSelection();
 })
 
+
+
+
+//##########INTERFACE END#########//
+
+//##########BUSSINESS################//
+const { get } = require("jquery");
+
+var heroes = new Array;
+const powerstats =  ["combat","durability","intelligence","power","speed","strength"]  
+var heroN = 1;
+var heroUser = 1;
+var tournamentIDs = new Array;
+var winners = new Array;
+
+
+function getHeroes(){
+    for(let i=0; i<10 ;i++){ 
+        var id = Math.floor(Math.random()*733);
+        $.getJSON("https://superheroapi.com/api/2983210371755036/"+ id , (data, status) => {  
+            heroes.push(data);      
+        })
+            .done(function(data){
+                let j = 1
+                $(".hero"+(j+i)).attr("src",data.image.url);
+                $(".hero"+(j+i)).attr("alt",data.name);
+                $(".hero"+(j+i)+"-name").text(data.name);
+                $(".hero"+(j+i)).attr("title", data.name);
+            })
+    } 
+}
+
+let promise = new Promise((resolve,reject) => {
+    getHeroes();
+    if (heroes != undefined){
+        resolve("Got data")
+    }else if(heroes[0] == undefined) {
+        reject("Get failed")
+    }
+})
+
+promise.then((message) =>{
+    console.log(message)
+    console.log(heroes)
+}).catch((message)=>{
+    console.log(message)
+    alert("Failed to get data from database. Please try again.")
+})
+
+
+function selectHeroes(){
+    tournamentIDs = [0,1,2,3,4,5,6,7,8,9];
+    let newPosition, temp;
+    for(let i = tournamentIDs.length -1; i>0;i--){  
+        newPosition = Math.floor(Math.random()*(i+1));
+        temp = tournamentIDs[i];
+        tournamentIDs[i] = tournamentIDs[newPosition];
+        tournamentIDs[newPosition] = temp;
+    } 
+    tournamentIDs.length = 7;
+    tournamentIDs.unshift(heroUser); 
+    console.log("tournamentIDs: "+tournamentIDs);
+    return tournamentIDs;
+}
+
+
+
+
+async function fillTournament(){
+    
+    await selectHeroes();
+    for(let i = 0;i<8;i++){
+        $(".tournament-container .hero"+(i)).attr("src",heroes[tournamentIDs[i]].image.url); 
+        $(".tournament-container .hero"+(i)).attr("title",heroes[tournamentIDs[i]].name); 
+        /* console.log(heroes[tournamentIDs[i]]); */
+    }
+    battle(0,1)
+}
+
+function round(i1, i2){
+    let stat1 = 0;
+    let stat2 = 0;
+    stat1 = powerstats[Math.floor(Math.random() * (5 - 0) + 0)]
+    stat2 = powerstats[Math.floor(Math.random() * (5 - 0) + 0)]
+    while(stat1 == stat2){
+        stat2 = powerstats[Math.floor(Math.random() * (5 - 0) + 0)];
+    }
+    if(( heroes[i1].powerstats[stat1] + heroes[i1].powerstats[stat2]) > (heroes[i2].powerstats[stat1] + heroes[i2].powerstats[stat2])){
+        console.log("prvi")
+        return true; //prvi je pobijedio
+
+    }else if (( heroes[i1].powerstats[stat1] + heroes[i1].powerstats[stat2]) < (heroes[i2].powerstats[stat1] + heroes[i2].powerstats[stat2])){
+        console.log("drugi")
+        return false; //drugi je pobijedio
+    }
+}
+
+
+function battle(i1,i2){
+
+    var heroes1score = 0;
+    var heroes2score = 0;
+    while(heroes1score <2 && heroes2score <2){
+        if(round(i1,i2)){
+            heroes1score += 1
+        }else if (round(i1,i2)==false){
+            heroes2score += 1
+        }
+    }
+    
+    if(heroes1score > heroes2score){
+        winners.push(heroes[i1]);
+    }else if (heroes1score < heroes2score){
+        winners.push(heroes[i2]);
+    }
+    console.log(winners);
+    console.log(heroes1score +":"+ heroes2score);
+}
+
+
+
+//##########BUSSINESS END################//
